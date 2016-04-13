687fa869558c3fccd88e2f4d8a6540134c2b5522
jasonhillier <jason@paviasystems.com>
2016-04-13 16:29:10 -0700

    bower publish

--- bower_publish.sh
+++ bower_publish.sh
@@ -2,11 +2,11 @@
 
 cd NestedSortedMap
 npm install
-browserify index.js -o ./dist/main.js
+browserify index.js -o ../dist/main.js
 
 cd ..
 git commit -am "bower publish"
-git tag v1.1
-git push origin v1.1
+git tag v1.2
+git push origin v1.2
 
 #bower register headlight-components https://github.com/paviasystems/headlight-components.git
