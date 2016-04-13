80d44af240773773950645d6e39c2b0f3ef19dcc
jasonhillier <jason@paviasystems.com>
2016-04-13 16:30:28 -0700

    bower publish

--- bower_publish.sh
+++ bower_publish.sh
@@ -6,7 +6,7 @@ browserify index.js -o ../dist/main.js
 
 cd ..
 git commit -am "bower publish"
-git tag v1.2
-git push origin v1.2
+git tag v1.2.0
+git push origin v1.2.0
 
 #bower register headlight-components https://github.com/paviasystems/headlight-components.git
